c04fb7ada5d619b3dd1ae7b2113089b90ab7e408
Hiro-guitar <journey.hiroki@gmail.com>
2025-07-12 21:17:04 +0900

    Update suumo_scraper.py

--- suumo_scraper.py
+++ suumo_scraper.py
@@ -10,7 +10,9 @@ import re
 
 def login_to_fn_forrent(login_id: str, password: str):
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
 
     driver.get("https://www.fn.forrent.jp/fn/")
